7e8477742ce3b52623693e0fa77738887859e844
tialaron <30555530+tialaron@users.noreply.github.com>
2022-08-12 16:55:17 +0300

    Update stramspacy.py

--- stramspacy.py
+++ stramspacy.py
@@ -51,4 +51,7 @@ st.markdown('''<h1 style='text-align: center; color: black;'
 st.write('Нейронная сеть, представленная здесь, обучена распзнавать документы в виде JPG файлов.')
 st.write('Вы можете выбрать любой документ из представленных в списке для распознавания.')
 option1 = st.selectbox('Какой документ Вы выбираете?',('ИНН','СНИЛС','полис омс','паспорт1','паспорт2'))
+full_path = path_pict+option1+'_ЧБ.png'
+img = Image.open(full_path)
+st.image(full_path)
 
